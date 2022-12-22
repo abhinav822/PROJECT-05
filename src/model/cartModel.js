db725fbcd1b9e35f4f8666583d4609c96952843b
Abhinav Pratap Singh <pabhinav400@gmail.com>
2022-12-22 11:15:00 +0530

    Create cartModel.js

---
+++ src/model/cartModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const cartSchema = new mongoose.Schema({
+
+    userId: {
+        type: ObjectId,
+        required: true,
+        ref: 'User',
+        unique: true
+    },
+    
+    items: [{
+        productId: { type: ObjectId, ref: 'Product', required: true },
+        quantity: { type: Number, required: true, min: 1 },
+    }],
+
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    totalItems: {
+        type: Number,
+        required: true
+    }
+
+}, { timestamps: true })
+
+
+module.exports = mongoose.model('Cart', cartSchema)
